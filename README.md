0afe829ebb40a70b123e532dc4dcfae8a702ed49
MRaseem <132068035+MRaseem@users.noreply.github.com>
2024-08-04 15:45:41 +0400

    4-Aug

---
+++ README.md
@@ -0,0 +1,22 @@
+# Simple-Quiz-App-Flutter
+A Quiz app using Flutter.
+
+- Learned widgets(Stateful,Stateless), styling & positioning.
+
+## App Demo
+https://user-images.githubusercontent.com/53289959/155473046-4fedbc07-5556-4d99-9b80-d93cd4ec95be.mp4
+
+## Getting Started
+
+This project is a starting point for a Flutter application.
+
+A few resources to get you started if this is your first Flutter project:
+
+- [Lab: Write your first Flutter app](https://flutter.dev/docs/get-started/codelab)
+- [Cookbook: Useful Flutter samples](https://flutter.dev/docs/cookbook)
+
+For help getting started with Flutter, view our
+[online documentation](https://flutter.dev/docs), which offers tutorials,
+samples, guidance on mobile development, and a full API reference.
+# QuizAPP
+# QuizAPP
---
+++ lib/result.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/material.dart';
+
+class Result extends StatelessWidget {
+  final int resultScore;
+  final Function resetQuiz;
+
+  Result(this.resultScore, this.resetQuiz);
+
+  String get resultPhrase {
+    String result;
+
+    if (resultScore <= 40) {
+      result = "You are awesome Person.";
+    } else if (resultScore <= 60) {
+      result = "Pretty likeable";
+    } else if (resultScore <= 80) {
+      result = "You are ... strange?!";
+    } else {
+      result = "You are so bad!";
+    }
+
+    return result;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Column(
+        children: [
+          Text(
+            resultPhrase,
+            style: const TextStyle(
+              fontSize: 36,
+              fontWeight: FontWeight.bold,
+            ),
+            textAlign: TextAlign.center,
+          ),
+          TextButton(
+            onPressed: () => resetQuiz(),
+            child: const Text("Restart Quiz!"),
+          )
+        ],
+      ),
+    );
+  }
+}
